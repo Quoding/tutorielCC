bc8518f16c7b36ff4c9083838a12fc088c42cb68
Quoding <quoding@protonmail.ch>
2022-04-29 12:31:09 -0400

    Add batch_script and logs dir

---
+++ batch_script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=def-sponsor00
+#SBATCH --mem=2G                                        # increase as needed
+#SBATCH --mail-type=END,FAIL                            # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=youremail@whatever.com              # Where to send mail
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1                               # Run on a single CPU
+#SBATCH --output=logs/slurm-%j.log                      # Output path for logs, %j is the jobid
+
+module load python/3.8
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r requirements.txt
+
+#pip install -e /home/user001/.../de-torch # Exemple de package non supporté, on doit utiliser pip -e
+
+python script.py
