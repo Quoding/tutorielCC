b0720646866310ffef797ccde9dcad65ce02b76d
Quoding <quoding@protonmail.ch>
2022-04-29 11:33:01 -0400

    Add simple python script and requirements file

---
+++ script.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+class TestModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layers = nn.Sequential(
+                    nn.Linear(32, 16),
+                    nn.Linear(16, 8),
+                    nn.Linear(8, 1)
+                )
+
+    def forward(self, x):
+        return self.layers(x)
+
+model = TestModel()
+
+print("Model layers:")
+print(model.layers)
+print()
+
+tns = torch.normal(0, 1, (32,))
+print("Random tensor:")
+print(tns)
+print()
+
+print("Model output:")
+print(model(tns))
